5a9b3f855c85f2b0c0c62740a2a47823b2fce9b5
Mike Burns <mike.burns@upstatement.com>
2020-01-15 10:13:04 -0500

    add notes on if expressions

--- ch3/branches/src/main.rs
+++ ch3/branches/src/main.rs
@@ -1,9 +1,11 @@
 fn main() {
-    let number = 3;
+    let number = 7;
 
+    // conditions *must* be bool
     if number < 5 {
+        // we've entered a branch or 'arm' of code
         println!("condition was true");
-    } else {
+    } else { // else is optional
         println!("condition was false");
     }
 }
