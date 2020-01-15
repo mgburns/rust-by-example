ef534ea8e85fa6d679745212b7f8c0e64733b362
Mike Burns <mike.burns@upstatement.com>
2020-01-15 10:07:01 -0500

    Add function with return value

--- ch3/functions/src/main.rs
+++ ch3/functions/src/main.rs
@@ -3,13 +3,19 @@ fn main() {
     let y = {
         let x = 3; // let x is a statement, 3 is an expression that resolves to the value 3
         x + 1 // expressions don't include ending semicolons, so this will be assigned to y
-    }; // this block is also an expression!
+    }; // this {} block is an expression evaluating to 4
 
     another_function(5, 6);
+    println!("Five: {}", five());
 }
 
 // in function signatures you must declare the type of each parameter
 fn another_function(x: i32, y: i32) {
     println!("The value of x is: {}", x);
     println!("The value of y is: {}", y);
+}
+
+// return values must have a declared type, but don't need a name
+fn five() -> i32 {
+    5
 }
