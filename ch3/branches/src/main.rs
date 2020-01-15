c37b19557b65e502082bf2de51103a811334e9d5
Mike Burns <mike.burns@upstatement.com>
2020-01-15 10:15:27 -0500

    Update if expression to use else if

--- ch3/branches/src/main.rs
+++ ch3/branches/src/main.rs
@@ -1,11 +1,16 @@
 fn main() {
-    let number = 7;
+    let number = 6;
 
     // conditions *must* be bool
-    if number < 5 {
+    if number % 4 == 0 {
         // we've entered a branch or 'arm' of code
-        println!("condition was true");
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        // add as many `else if`'s as you want
+        println!("number is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("number is divisible by 2");
     } else { // else is optional
-        println!("condition was false");
+        println!("number is not divisible by 2, 3, or 4");
     }
 }
