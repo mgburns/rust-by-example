fd675c7536bca47e4d3fa08e50727acb6874e685
Mike Burns <mike.burns@upstatement.com>
2020-01-19 09:31:38 -0500

    Add dir for ch5 (structs)

---
+++ ch5/README.md
@@ -0,0 +1,4 @@
+# 5. Using structs to structure related data
+
+> A struct, or structure, is a custom data type that lets you name and package together multiple related values that make up a meaningful group.
+
---
+++ ch5/structs/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    let user1 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("someusername123"),
+        active: true,
+        sign_in_count: 1,
+    };
+    println!("{:?}", user1);
+}
+
+#[derive(Debug)]
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
+}
