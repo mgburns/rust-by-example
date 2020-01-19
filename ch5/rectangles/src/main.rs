aa95ba27a92070810edc8119122881062dd5ab8b
Mike Burns <mike.burns@upstatement.com>
2020-01-19 10:02:51 -0500

    Refactor area fn to Rectangle method

--- ch5/rectangles/src/main.rs
+++ ch5/rectangles/src/main.rs
@@ -4,6 +4,12 @@ struct Rectangle {
     height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
 fn main() {
     let rect1 = Rectangle {
         width: 30,
@@ -13,10 +19,7 @@ fn main() {
 
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(&rect1)
+        rect1.area();
     );
 }
 
-fn area(rect: &Rectangle) -> u32 {
-    rect.width * rect.height
-}
