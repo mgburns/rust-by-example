0495fcb733c43e0ec9543239029c9b31324bc71c
Mike Burns <mike.burns@upstatement.com>
2020-01-20 07:46:50 -0500

    Add example Message enum

--- ch6/enums/src/main.rs
+++ ch6/enums/src/main.rs
@@ -4,7 +4,22 @@ enum IpAddr {
     V6(String),
 }
 
+enum Message {
+    Quit, // no data
+    Move { x: i32, y: i32 }, // anonymous struct
+    Write(String), // String
+    ChangeColor(i32, i32, i32), // Three integers
+}
+
+impl Message {
+    // enums can have methods, just like structs
+    fn call(&self) {}
+}
+
 fn main() {
     let four = IpAddr::V4(127, 0, 0, 1);
     let six = IpAddr::V6(String::from("::1"));
+
+    let m = Message::Write(String::from("hello"));
+    m.call();
 }
